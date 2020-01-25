efd967bdbf9c73b8a83a3dd78518ec5816a832ba
CrysiscCaramel <danilukas@mail.ru>
2020-01-25 13:42:30 +0300

    create docker-compose and connect TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'changeme',
+      database: 'postgres',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
